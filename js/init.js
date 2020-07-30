9b2ce0cec6e71ddf9cbb7dd6fd10a8ee03459651
EGG\Lou Madelo <lmadelo@yondu.com>
2020-07-30 22:56:05 +0800

    initial commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel').carousel({
+    	indicators: true
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
